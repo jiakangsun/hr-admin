48223cc87ab482dc60f42894919c8044c485a9e2
jiakangsun <1064491889@qq.com>
2022-07-13 11:50:40 +0800

    项目初始化

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: '人力资源后台管理',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   * 设置appMain右侧主体上面的header 是否固定定位
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   * 设置侧边栏上的logo是否显示
+   */
+  sidebarLogo: true
+}
